8a1aec5abb7a7593da4ed7890ee8d3dc2b1da78d
Stepanov Valentin <falken.ua@gmail.com>
2016-11-27 22:46:01 +0200

    Init commit

---
+++ VirtualLaboratoryPI/VirtualLaboratoryPI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
+
+namespace VirtualLaboratoryPI.ViewModels
+{
+    public class LoginViewModel: ViewModelBase
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public RelayCommand SignInCommand {
+            get
+            {
+                return new RelayCommand(SignIn);
+            }
+        }
+
+        private void SignIn()
+        {        }
+    }
+}
